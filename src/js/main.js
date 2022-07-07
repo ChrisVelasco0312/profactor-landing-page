382e4bf25f1938b8a772f2f9f26b67f796145889
ChrisVelasco0312 <cristianvelasco0312@gmail.com>
2022-07-07 16:40:25 -0500

    add ciphers

--- src/js/main.js
+++ src/js/main.js
@@ -2,94 +2,94 @@
 const returnAppear = (trigger, duration, direction) => {
 
   const properties = (x, y) => [
-        {
-          x:x,
-          y:y,
-          opacity: 0
-        },
-        {
-          duration: duration, 
-          x: 0,
-          y: 0,
-          opacity: 1,
-          scrollTrigger: {
-            trigger: trigger,
-            start: '-500px',
-            end:'+=200px',
-            delay: 1,
-            scrub: 1,
-            anticipatePin: 1,
-          }
-        }
-      ]
+    {
+      x: x,
+      y: y,
+      opacity: 0
+    },
+    {
+      duration: duration,
+      x: 0,
+      y: 0,
+      opacity: 1,
+      scrollTrigger: {
+        trigger: trigger,
+        start: '-500px',
+        end: '+=200px',
+        delay: 1,
+        scrub: 1,
+        anticipatePin: 1,
+      }
+    }
+  ]
 
-  if(direction == "bottom") {
-      return properties(0, 100)  
+  if (direction == "bottom") {
+    return properties(0, 100)
   }
-  if(direction == "top") {
-      return properties(0, -100)  
+  if (direction == "top") {
+    return properties(0, -100)
   }
-  if(direction == "left") {
-      return properties(-100, 0)  
+  if (direction == "left") {
+    return properties(-100, 0)
   }
-  if(direction == "default") {
-      return properties(0, 0)  
+  if (direction == "default") {
+    return properties(0, 0)
   }
 }
 
 const defaultAppear = [
-        {
-          opacity: 0,
-        },
-        {
-          duration: 1, 
-          opacity: 1,
-        }
-      ]
+  {
+    opacity: 0,
+  },
+  {
+    duration: 1,
+    opacity: 1,
+  }
+]
 
 
 //initial
-gsap.fromTo(".hero .title", 
+gsap.fromTo(".hero .title",
   {
     opacity: 0,
     y: 50
   },
   {
-    duration:1,
+    duration: 1,
     opacity: 1,
     y: 0
   }
 )
 
-gsap.fromTo(".hero .hero-link", 
+gsap.fromTo(".hero .hero-link",
   {
     opacity: 0,
   },
   {
-    duration:1,
+    duration: 1,
     opacity: 1,
   }
 )
 
 //about
-gsap.fromTo("#about .titles", 
+gsap.fromTo("#about .titles",
   ...returnAppear('#about', 2, 'top')
 )
 
-gsap.fromTo("#about p.paragraph", 
+gsap.fromTo("#about p.paragraph",
   ...returnAppear('#about', 3, 'left')
 )
 
-gsap.fromTo("#about .about-who__image", 
+gsap.fromTo("#about .about-who__image",
   ...returnAppear('#about', 3, 'bottom')
 )
 
 //about-us
-gsap.fromTo(".about-us .titles", 
+gsap.fromTo(".about-us .titles",
   ...returnAppear('.about-us .titles', 2, 'bottom')
 )
 
-gsap.fromTo(".about-us .team-card", 
+gsap.fromTo(".about-us .team-card",
   ...returnAppear('.about-us .titles', 3, 'bottom')
 )
 
@@ -112,23 +112,28 @@ gsap.fromTo(".our-projects .our-projects__card",
 )
 
 document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-  anchor.addEventListener('click', function (e) {
+  anchor.addEventListener('click', function(e) {
     e.preventDefault();
     document.querySelector(this.getAttribute('href')).scrollIntoView({
       behavior: 'smooth'
     });
 
-  //   gsap.to(window, {
-		// 	scrollTo: {
-		// 		y: 100,
-		// 		autoKill: false
-		// 	},
-		// 	duration: 1
-		// });
+    //   gsap.to(window, {
+    // 	scrollTo: {
+    // 		y: 100,
+    // 		autoKill: false
+    // 	},
+    // 	duration: 1
+    // });
     console.log("triggered")
   });
 })
 
+const splide = new Splide('.splide', {
+  perPage: 4,
+  rewind: true
+})
 
+splide.mount()
 
 
